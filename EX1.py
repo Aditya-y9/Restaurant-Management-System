b5258a73fec6a877695eccb8481d81a6c840b6e9
Aditya-y9 <aditya.yedurkar@gmail.com>
2023-09-09 15:02:30 +0530

    Adding first basic file

---
+++ EX1.py
@@ -0,0 +1,21 @@
+# pip to install libraries(eg pygame,tensorflow,scilearn)
+# modules are code written by else which we can use
+
+# comment / uncomment with (Ctrl + /)
+
+# \n new line
+# \" double quote escape
+
+print("Not all \"Adityas\" are Adityas")
+
+
+print("Aditya",7,6)
+# by default separator is sep = " ".
+# end = "" to escapenew line.
+
+print("Aditya",7,6,sep="%")
+
+print("Aditya",7,6,end="")
+print("Aditya")
+
+
