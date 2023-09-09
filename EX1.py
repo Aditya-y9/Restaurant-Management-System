8aa4ca5f42cd433453ef89206efb50424f256dc6
Aditya-y9 <aditya.yedurkar@gmail.com>
2023-09-09 15:08:24 +0530

    Started OOPs practice

--- EX1.py
+++ EX1.py
@@ -18,4 +18,19 @@ print("Aditya",7,6,sep="%")
 print("Aditya",7,6,end="")
 print("Aditya")
 
+# dictionaries
+# oops
+ 
+class Aditya:
+    name=""
+    age=0
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    
+
+
+obj = Aditya("aditya", 19)
+print(obj.name)
+
 
