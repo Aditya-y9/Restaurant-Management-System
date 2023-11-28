30fa7af7ca12881a107561511e86d35fa5454007
Aditya-y9 <aditya.yedurkar@gmail.com>
2023-11-28 23:21:16 +0530

    Adding Calculator fromLAB

---
+++ PD_LAB/Calc.py
@@ -0,0 +1,95 @@
+import math
+
+equation = ""
+
+
+
+def clear():
+    global equation
+    equation = ""
+    return equation
+
+
+def show(value):
+    global equation
+    equation += value
+    return equation
+
+def calculate():
+    global equation
+    result = ""
+    if equation!= "":
+        try:
+            result = eval(equation)
+        except:
+            result = "Error"
+
+    return result
+
+def modulo():
+    global equation
+    equation += "%"
+    return equation
+
+def mod():
+    global equation
+    equation = "abs(" + equation + ")"
+    return equation.replace("math.modf","mod")
+
+def sqrt():
+    global equation
+    equation = "math.sqrt(" + equation + ")"
+    return equation.replace("math.sqrt","√")
+
+def sin():
+    global equation
+    eq = equation
+    clear()
+    equation += "math.sin(" + str(float(eq)*math.pi/180) + ")"
+    return equation
+
+def cos():
+    global equation
+    eq = equation
+    clear()
+    equation += "math.cos(" + str(float(eq)*math.pi/180) + ")"
+    return equation
+
+def tan():
+    global equation
+    eq = equation
+    clear()
+    equation += "math.tan(" + str(float(eq)*math.pi/180) + ")"
+    return equation
+
+def exp():
+    global equation
+    equation = "math.exp(" + equation + ")"
+    return equation.replace("math.exp","e^")
+
+def power():
+    global equation
+    equation += "**"
+    return equation
+
+def inverse():
+    global equation
+    equation = "1/(" + equation + ")"
+    return equation
+
+def log10():
+    global equation
+    equation = "math.log10(" + equation + ")"
+    return equation.replace("math.log10","log")
+
+def log():
+    global equation
+    equation = "math.log(" + equation + ")"
+    return equation.replace("math.log","ln")
+
+def fact():
+    global equation
+    equation = "math.factorial(" + equation + ")"
+    return equation.replace("math.factorial(","").replace(")","!")
+
+
--- PD_LAB/Programs/Calculator.py
+++ PD_LAB/Programs/Calculator.py
@@ -12,13 +12,14 @@ root.title("PD LAB")
 # to set the size of the window (fixed)
 
 # geometry("width x height + X_POS + Y_POS")
-root.geometry("850x600+200+25")
+root.geometry("900x600+200+25")
 
 # resizable h?
 root.resizable(False, False)
 
 # to set the background color of the window
-root.configure(bg="#2A2D36")
+# .configure() method is used to configure the widget specific parameters
+root.configure(bg="#2A2D36",border=10,borderwidth=10,relief="sunken")
 
 # init screen empty
 equation = ""
@@ -99,9 +100,13 @@ def show(value):
     label_result.configure(text=equation)
 
 def clear():
+    # assigin pointer to global var
     global equation
+
     # reset the equation string
     equation = ""
+
+    # update the label
     label_result.configure(text=equation)
 
 def calculate():
@@ -127,39 +132,39 @@ label_result = tk.Label(root, width=40, height=2, font=("Arial", 20),fg="#47f507
 label_result.pack()
 
 # to create Buttons
-tk.Button(root,text="C",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:clear()).place(x=10,y=100)
-tk.Button(root,text="[]",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("/")).place(x=150,y=100)
-tk.Button(root,text="%",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("%")).place(x=290,y=100)
-tk.Button(root,text="*",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("*")).place(x=430,y=100)
+tk.Button(root,text="C",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:clear()).place(x=10,y=100)
+tk.Button(root,text="[]",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("/")).place(x=150,y=100)
+tk.Button(root,text="%",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("%")).place(x=290,y=100)
+tk.Button(root,text="*",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("*")).place(x=430,y=100)
 
-tk.Button(root,text="7",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("7")).place(x=10,y=200)
-tk.Button(root,text="8",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("8")).place(x=150,y=200)
-tk.Button(root,text="9",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("9")).place(x=290,y=200)
-tk.Button(root,text="-",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("-")).place(x=430,y=200)
+tk.Button(root,text="7",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("7")).place(x=10,y=200)
+tk.Button(root,text="8",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("8")).place(x=150,y=200)
+tk.Button(root,text="9",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("9")).place(x=290,y=200)
+tk.Button(root,text="-",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("-")).place(x=430,y=200)
 
-tk.Button(root,text="4",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("4")).place(x=10,y=300)
-tk.Button(root,text="5",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("5")).place(x=150,y=300)
-tk.Button(root,text="6",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("6")).place(x=290,y=300)
-tk.Button(root,text="+",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("+")).place(x=430,y=300)
+tk.Button(root,text="4",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("4")).place(x=10,y=300)
+tk.Button(root,text="5",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("5")).place(x=150,y=300)
+tk.Button(root,text="6",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("6")).place(x=290,y=300)
+tk.Button(root,text="+",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("+")).place(x=430,y=300)
 
-tk.Button(root,text="1",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("1")).place(x=10,y=400)
-tk.Button(root,text="2",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("2")).place(x=150,y=400)
-tk.Button(root,text="3",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("3")).place(x=290,y=400)
-tk.Button(root,text="0",width=11,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show("0")).place(x=10,y=500)
+tk.Button(root,text="1",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("1")).place(x=10,y=400)
+tk.Button(root,text="2",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("2")).place(x=150,y=400)
+tk.Button(root,text="3",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("3")).place(x=290,y=400)
+tk.Button(root,text="0",width=11,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show("0")).place(x=10,y=500)
 
-tk.Button(root,text=".",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="#f80",command=lambda:show(".")).place(x=290,y=500)
-tk.Button(root,text="=",width=5,height=3,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : calculate()).place(x=430,y=400)
+tk.Button(root,text=".",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="#f80",command=lambda:show(".")).place(x=290,y=500)
+tk.Button(root,text="=",width=5,height=3,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : calculate()).place(x=430,y=400)
 
-tk.Button(root,text="(",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : show("(")).place(x=570,y=100)
-tk.Button(root,text=")",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : show(")")).place(x=570,y=200)
+tk.Button(root,text="(",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : show("(")).place(x=570,y=100)
+tk.Button(root,text=")",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : show(")")).place(x=570,y=200)
 
-tk.Button(root,text="sin",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : sin("sin(")).place(x=570,y=300)
-tk.Button(root,text="cos",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : cos("cos(")).place(x=570,y=400)
-tk.Button(root,text="tan",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : tan()).place(x=570,y=500)
+tk.Button(root,text="sin",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : sin()).place(x=570,y=300)
+tk.Button(root,text="cos",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : cos()).place(x=570,y=400)
+tk.Button(root,text="tan",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : tan()).place(x=570,y=500)
 
-tk.Button(root,text="DEL",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : erase()).place(x=710,y=100)
-tk.Button(root,text="√",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : sqrt()).place(x=710,y=200)
-tk.Button(root,text="²",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : square()).place(x=710,y=300)
-tk.Button(root,text="^",width=5,height=1,font=("Arial",30,"bold"),bd=1,fg="#2A2D36",bg="blue",command=lambda : power()).place(x=710,y=400)
+tk.Button(root,text="DEL",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : erase()).place(x=710,y=100)
+tk.Button(root,text="√",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : sqrt()).place(x=710,y=200)
+tk.Button(root,text="²",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : square()).place(x=710,y=300)
+tk.Button(root,text="^",width=5,height=1,font=("Arial",30,"bold"),bd=5,fg="#2A2D36",bg="blue",command=lambda : power()).place(x=710,y=400)
 
 root.mainloop()
